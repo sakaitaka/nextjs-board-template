f587e96549983e418a9dc596c092858c5d8af7b2
sakaitaka <sakai.takahiro.22@shizuoka.ac.jp>
2023-06-08 16:56:14 +0900

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main}`}>
-        <p>Hello, world!</p>
+        <p>🐹🐹🐹🐹🐹🐹🐹🐹🐹🐹🐹🐹</p>
       </main>
     </>
   );
